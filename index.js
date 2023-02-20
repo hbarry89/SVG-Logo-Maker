18ab0253b9c63b9474b5b3244ca66221e7927e65
@hudabarry-wwMZPT <hudabarry@yahoo.com>
2023-02-19 20:48:30 -0500

    add questions

--- index.js
+++ index.js
@@ -2,3 +2,29 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// An array of questions for user input
+
+const questions = [
+    {
+        type: 'input',
+        message: 'What would you like to write on your logo? (up to three characters only)',
+        name: 'text',
+      },
+      {
+        type: 'input',
+        message: 'What would you like the text color to be? (color keyword OR a hexadecimal number)',
+        name: 'textColor',
+      },
+      {
+        type: 'list',
+        message: 'Select the shape you would like for your for your logo.',
+        name: 'shape',
+        choices: ['circle', 'triangle', 'square']
+      },
+      {
+          type: 'input',
+          message: 'What would you like the shape color to be? (color keyword OR a hexadecimal number)',
+          name: 'shapeColor',
+      },
+];
